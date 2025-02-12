97f3734e7f10081185d0ad8cc94b064956d763a6
Nastya Budanova <nanabudana@gmail.com>
2025-02-12 15:30:33 +0000

    1

---
+++ cp/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/labs_os-4/cp/client.cpp" "/workspaces/labs_os-4/cp/CMakeFiles/client.dir/client.cpp.o"
+  "/workspaces/labs_os-4/cp/lib/GameManager.cpp" "/workspaces/labs_os-4/cp/CMakeFiles/client.dir/lib/GameManager.cpp.o"
+  "/workspaces/labs_os-4/cp/lib/utils.cpp" "/workspaces/labs_os-4/cp/CMakeFiles/client.dir/lib/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cp/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.dir/client.cpp.o"
+  "CMakeFiles/client.dir/lib/GameManager.cpp.o"
+  "CMakeFiles/client.dir/lib/utils.cpp.o"
+  "client"
+  "client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
