52fe4fee7c47938107f7bcc2adb81ed80aabbb5b
Nastya Budanova <nanabudana@gmail.com>
2025-03-09 03:55:56 +0000

    endd

--- _deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/labs_os-1/_deps/googletest-src/googletest
-# Build directory: /workspaces/labs_os-1/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
