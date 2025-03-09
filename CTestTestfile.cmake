e29f187f78a318020243a11eb1d400c3794b1bb9
Nastya Budanova <nanabudana@gmail.com>
2025-03-09 04:40:15 +0000

    enddd

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/labs_os-1
-# Build directory: /workspaces/labs_os-1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
-subdirs("lab5-7")
--- lab5-7/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/labs_os-1/lab5-7
-# Build directory: /workspaces/labs_os-1/lab5-7
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
